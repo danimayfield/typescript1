f5831205e20b4aeb22c21654542c0dce6fe22077
Daniella Gagliardi <daniellagagliardi@Daniellas-MacBook-Pro.local>
2022-03-15 13:31:25 -0400

    finished functionality with typescript app

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,28 +1,84 @@
-import React from 'react'
-import { Todo } from '../model'
-import {AiFillEdit, AiFillDelete} from 'react-icons/ai'
-import {MdDone} from 'react-icons/md'
-import './styles.css'
+import React, { useState, useRef, useEffect } from "react";
+import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
 
 type Props = {
-    todo: Todo,
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
-    key: number
-}
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  key: number;
+};
 
+const SingleTodo: React.FC<Props> = ({ todo, todos, key, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-const SingleTodo: React.FC<Props> = ({todo, todos, key, setTodos }) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e:React.FormEvent, id:number) => {
+    e.preventDefault();
+
+    setTodos(todos.map((todo) => (
+      todo.id === id ? {...todo, todo:editTodo}: todo
+
+    )))
+    setEdit(false)
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit])
+  
+
   return (
-    <form className='todos__single' key={key}>
-        <span className='todos__single--text'>{todo.todo}</span>
-        <div>
-            <span className="icon"><AiFillEdit/></span>
-            <span className="icon"><AiFillDelete/></span>
-            <span className="icon"><MdDone/></span>
-        </div>
-    </form>
-  )
-}
+    <form className="todos__single" key={key} onSubmit={(e)=> handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          type="text"
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos__single--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
 
-export default SingleTodo
+      <div>
+        <span className="icon">
+          <AiFillEdit
+            onClick={() => {
+              if (!edit && !todo.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,45 @@
 export interface Todo {
-    id: number,
-    todo: string,
-    isDone: boolean,
+  id: number;
+  todo: string;
+  isDone: boolean;
 }
+
+// ! Unfinished reducer example:
+
+// type Actions =
+//   | {
+//       type: "add";
+//       payload: string;
+//     }
+//   | {
+//       type: "remove";
+//       payload: number;
+//     }
+//   | {
+//       type: "done";
+//       payload: number;
+//     };
+
+// import React, { useReducer } from "react";
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//     switch (action.type) {
+//         case "add":
+//             return [
+//                 ...state,
+//                 {id: Date.now(), todo: action.payload, isDone: false},
+//             ];
+//         case "remove" :
+//             return state.filter((todo)=> todo.id !== action.payload);
+//         case "done" :
+//             return state.map((todo) => todo.id !== action.payload ? {...todo, isDone: !todo.isDone})
+//     }
+// };
+
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+
+//   return <div></div>;
+// };
+
+// export default ReducerExample;
